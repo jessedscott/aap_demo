dd420fa0d77f5e3d3215dc3b18a4738b44ad251d
jessedscott <jessedscott@gmail.com>
2025-08-13 17:01:34 -0400

    Remove unecessary bash script

--- update_controller/update.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-pip install ansible-navigator
-
-ansible-galaxy collection install -r collections/requirements.yml
-
-ansible-navigator run -m stdout --pp missing --eei ee-supported-rhel8 resources.yml 
